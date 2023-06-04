f7b78f48cbe948e1a0db9a0a7a52eea5ff4d869f
aarnavpbhat <109448062+aarnavpbhat@users.noreply.github.com>
2023-06-04 13:15:26 -0700

    final

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,25 +1,40 @@
+import React from 'react';
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
 
-const Tab2: React.FC = () => {
+const About: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>About</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        <div className="about-page">
+          <div className="product-info">
+            <h2 className="product-name">Greenery</h2>
+            <p className="product-description">Greenery is a mobile application designed to help plant enthusiasts identify and learn about various plant species. Whether you're a seasoned botanist or just starting your journey into the world of plants, Greenery provides a user-friendly and informative experience.</p>
+          </div>
+
+          <div className="feature-info">
+            <h3 className="feature-title">Key Features:</h3>
+            <ul className="feature-list">
+              <li>
+                <strong>Plant Identification:</strong> Capture or upload a photo of a plant, and Greenery's advanced image recognition technology will quickly identify the plant species for you. Learn interesting facts about the plant and its characteristics.
+              </li>
+              <li>
+                <strong>Plant Library:</strong> Explore a comprehensive library of plant species, complete with detailed information, including common names, scientific names, the amount of water needed, and its livelihood. Discover new plants and expand your knowledge.
+              </li>
+              <li>
+                <strong>Personal Collection:</strong> Create and manage your own plant collection within the app. Keep track of the plants you've identified, save favorites, and add notes or photos to create a personalized plant journal.
+              </li>
+            </ul>
+          </div>
+        </div>
       </IonContent>
     </IonPage>
   );
 };
 
-export default Tab2;
+export default About;
